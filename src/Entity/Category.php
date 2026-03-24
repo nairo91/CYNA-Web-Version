38c8ce4a31b2af7f373b07e211ce153c4b3a4935
Orian Mirona <orian.mirona@gmail.com>
2026-03-24 09:33:14 +0100

    tentative migration

---
+++ src/Entity/Category.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+    ],
+    normalizationContext: ['groups' => ['category:read']],
+    order: ['name' => 'ASC'],
+)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['category:read', 'product:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    #[Groups(['category:read', 'product:read'])]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 120, unique: true)]
+    #[Groups(['category:read'])]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['category:read'])]
+    private ?string $image = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    #[Groups(['category:read'])]
+    private ?string $description = null;
+
+    /** @var Collection<int, Product> */
+    #[ORM\OneToMany(targetEntity: Product::class, mappedBy: 'category')]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /** @return Collection<int, Product> */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+}
